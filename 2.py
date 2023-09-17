ac5c93d0eab5be31ff41fcbb05f651f18fffd0f7
Lara S.Santana <124930864+contaLaraSSantana@users.noreply.github.com>
2023-09-17 18:19:57 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,12 +6,15 @@ while True:
     p= int(input("Você deseja registrar uma venda? Digite 1 para sim e 0 para não: "))
 
     if p == 1:
-        valor_de_peixes_vendidos = valor_de_peixes_vendidos+1
         peso= float(input("Qual o peso em gramas?: "))
-        quilo= peso/1000
-        valor= quilo*PRECO_QUILO_PEIXE
-        valor_total = valor_total+valor
-        print("O valor total dessa venda foi de:",valor)
+        if peso == 0:
+            print("Venda cancelada devido ao peso zero.")
+        else:
+            valor_de_peixes_vendidos = valor_de_peixes_vendidos+1
+            quilo= peso/1000
+            valor= quilo*PRECO_QUILO_PEIXE
+            valor_total = valor_total+valor
+            print("O valor total dessa venda foi de:",valor)
     elif p == 0:
         print("Registro de vendas cancelado")
         print("A quantidade de peixes vendidos foi: ",valor_de_peixes_vendidos)
